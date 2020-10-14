d4813c03ca818b22f5d4215046f0ebb82c257a8c
CodeHz <codehz@outlook.com>
2020-10-14 18:47:36 +0800

    improve tjs.h

--- src/tjs.h
+++ src/tjs.h
@@ -3,6 +3,14 @@
 #endif
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
+#define scoped(f) __attribute__((cleanup(f)))
+#define alignof __alignof__
+#define countof(x) (sizeof(x) / sizeof(x[0]))
+#define container_of(ptr, type, member) ({ \
+    const typeof(((type *)0)->member) *__mptr = (ptr); \
+    (type *)((char *)__mptr - offsetof(type,member)); \
+  })
 #ifdef __TJS_MEMORY__
 typedef struct __tjsvec_str {
   char *ptr;
@@ -38,11 +46,18 @@ extern bool tjs_notify_data(tjscallback cb, size_t num, tjscallback_data const *
     tjscallback_data __tmp_callback_data__[] = { list }; \
     tjs_notify_data(cb, sizeof(__tmp_callback_data__) / sizeof(tjscallback_data), __tmp_callback_data__); \
   })
-#define TJS_DATA_VOID(value) (tjscallback_data){ type: 0, i: (value) }
+#define TJS_DATA_VOID() (tjscallback_data){ type: 0 }
 #define TJS_DATA_INT(value) (tjscallback_data){ type: 1, i: (value) }
 #define TJS_DATA_DOUBLE(value) (tjscallback_data){ type: 2, d: (value) }
 #define TJS_DATA_STRING(value) (tjscallback_data){ type: 3, s: (value) }
 #define TJS_DATA_WSTRING(value) (tjscallback_data){ type: 4, w: (value) }
 #define TJS_DATA_VECTOR(value_ptr, value_len) (tjscallback_data){ type: 5, v: { ptr: value_ptr, len: value_len } }
 #define TJS_DATA_POINTER(value) (tjscallback_data){ type: 6, p: (value) }
+#define TJS_DATA(value) _Generic((value), \
+    int: TJS_DATA_INT(value),\
+    double: TJS_DATA_DOUBLE(value),\
+    char *: TJS_DATA_STRING(value),\
+    wchar_t *: TJS_DATA_WSTRING(value),\
+    void *: TJS_DATA_POINTER(value)\
+  )
 #endif
