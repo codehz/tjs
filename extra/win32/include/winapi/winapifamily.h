3a3a9b6c555ff3535df8c98677ea5bb4b6e520d5
CodeHz <codehz@outlook.com>
2020-10-25 14:11:06 +0800

    add more headers

---
+++ extra/win32/include/winapi/winapifamily.h
@@ -0,0 +1,9 @@
+/**
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER within this package.
+ */
+
+#ifndef _INC_WINAPIFAMILY
+#define _INC_WINAPIFAMILY
+
+#endif
